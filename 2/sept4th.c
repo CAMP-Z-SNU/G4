9f6173614813e9ada54b68454971898309d6e3b4
shadow100701 <shamithabolem@gmail.com>
2024-09-05 16:00:02 +0530

    Add files via upload

---
+++ 2/sept4th.c
@@ -0,0 +1,20 @@
+//PALINDROME
+
+#include<stdio.h>
+int main()
+{ int i,n,x,r,s=0;
+ printf("enter number=");
+ scanf("%d",&n);
+ x=n;
+ while(n>0)
+ {
+  r=n%10;
+  s=s*10+r;
+  n=n/10;
+ }
+ if(x==s)
+ printf("it is a palindrome");
+ else
+ printf("it is not a palindrome");
+ return 0;
+}
